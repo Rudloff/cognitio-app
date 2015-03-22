6bfd05cb3a6c2490522ace8b3b1f66eedf7ead32
Pierre Rudloff <contact@rudloff.pro>
2015-03-22 12:10:12 +0100

    Test pour la démo

--- www/js/test_mms.js
+++ www/js/test_mms.js
@@ -51,126 +51,6 @@ var test_mms = (function () {
                         ]
                     }
                 ]
-            },
-            {
-                title: "Apprentissage",
-                description: "Cette partie concerne la mémoire courte, c'est le stockage bref des informations.<br/><br/><b>Consignes</b> : Je vais vous donner une liste de 3 mots, j'aimerais que vous les répetiez et essayiez de les retenir car je vous les redemanderais tout à l'heure",
-                questions: [
-                    {
-                        title: "",
-                        description: "Si le sujet ne répète pas les trois mots au premier essai, les redonner jusqu'à ce qu'ils soient répétés correctement dans la limite de six essais. En effet, l'épreuve de rappel ne peut être analysée que si les trois mots ont été enregistrés.",
-                        questions: [
-                            {
-                                title: "clef"
-                            },
-                            {
-                                title: "citron"
-                            },
-                            {
-                                title: "ballon"
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                title: "Attention et calcul",
-                description: "Cette partie apprécie la fonction qui permet la sélection et le maintien d'une information",
-                questions: [
-                    {
-                        title: "",
-                        description: "Maintenant, je vais vous demander de compter à partir de 100 en retirant 7 à chaque fois jusqu'à ce que je vous arrête.<br/>(Il est permis d'aider le patient en lui présentant la première soustraction. « 100 – 7, combien cela fait-il ?  Et ensuite : Continuez... »)<br/><br/>Pour tous les sujets, même pour ceux qui ont obtenu le maximum de points, demander également : « Pouvez-vous épeler le mot MONDE à l'envers en commençant par la dernière lettre ? » (non noté)",
-                        questions: [
-                            {
-                                title: "93"
-                            },
-                            {
-                                title: "86"
-                            },
-                            {
-                                title: "79"
-                            },
-                            {
-                                title: "72"
-                            },
-                            {
-                                title: "65"
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                title: "Rappel",
-                description: "",
-                questions: [
-                    {
-                        title: "",
-                        description: "Quels étaient les trois mots que je vous ai demandé de retenir tout à l'heure ?",
-                        questions: [
-                            {
-                                title: "clef"
-                            },
-                            {
-                                title: "citron"
-                            },
-                            {
-                                title: "ballon"
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                title: "Langage",
-                description: "",
-                questions: [
-                    {
-                        title: "",
-                        description: "Montrer un crayon.  Quel est le nom de cet objet ?<br/><br/>(Aucune autre réponse que « crayon » n'est admise. Le sujet ne doit pas prendre les objets en main.)"
-                    },
-                    {
-                        title: "",
-                        description: "Montrer une montre.  Quel est le nom de cet objet ? "
-                    },
-                    {
-                        title: "Écoutez bien et répétez après moi : « PAS DE MAIS, DE SI, NI DE ET » ",
-                        description: "La phrase doit être prononcée lentement à haute voix, face au malade. Si le patient dit ne pas avoir entendu, ne pas répéter la phrase. Si l'examinateur a un doute, il peut être admis de vérifier en répétant la phrase à la fin du test."
-                    },
-                    {
-                        title: "",
-                        description: "La phrase doit être prononcée lentement à haute voix, face au malade. Si le patient dit ne pas avoir entendu, ne pas répéter la phrase. Si l'examinateur a un doute, il peut être admis de vérifier en répétant la phrase à la fin du test.<br/>Poser une feuille de papier blanc sur le bureau et la montrer au sujet en lui disant : « Écoutez bien et faites ce que je vais vous dire »<br/><br/>Si le sujet arrête et demande ce qu'il doit faire, ne pas répéter la consigne, mais dire « Faites ce que je vous ai dit de faire ».",
-                        questions: [
-                            {
-                                title: "« Prenez mon papier dans la main droite. »"
-                            },
-                            {
-                                title: "« Pliez-le en deux. »"
-                            },
-                            {
-                                title: "« Jetez-le par terre. »"
-                            }
-                        ]
-                    },
-                    {
-                        title: "",
-                        description: "Montrer la feuille de papier sur laquelle est écrit en gros caractères « FERMEZ LES YEUX » et dire :  « Faites ce qui est marqué.<br/>Le point n'est accordé que si le sujet ferme les yeux. Il n'est pas accordé s'il se contente de lire la phrase."
-                    },
-                    {
-                        title: "",
-                        description: "Montrer une feuille de papier et un stylo, en disant :  « Voulez-vous m'écrire une phrase, ce que vous voulez, mais une phrase entière. »<br/>La phrase contient au minimum un sujet et un verbe, ne pas tenir compte des erreurs d'orthographe ou de syntaxe."
-                    }
-                ]
-            },
-            {
-                title: "Construction",
-                description: "",
-                questions: [
-                    {
-                        title: "",
-                        description: "Montrer la figure et lui demander :  « Voulez-vous recopier ce dessin. »<br/><br/>Compter un point si tous les angles sont présents et si les figures se coupent sur les deux côtés différents. On peut autoriser plusieurs essais et accorder un temps d'une minute."
-                    }
-                ]
             }
         ],
         nbQuestions = 0,
@@ -229,10 +109,10 @@ var test_mms = (function () {
             $('#test_mms_question, #test_mms_intro, #test_mms_footer').hide();
             $('#test_mms_results').show();
             var color, scoreDesc;
-            if (score >= 26) {
+            if (score >= 10) {
                 color = '#88C100';
                 scoreDesc = "Rien d’alarmant pour le moment, cependant si vous avez le moindre doute, parlez-en à votre médecin traitant.";
-            } else if (score >=20) {
+            } else if (score >=3) {
                 color = '#FF8A00';
                 scoreDesc = "Oups ! Quelques petites questions posent problème, réessayez dans un mois, dans un environnement calme. Cependant si le moindre doute a motivé la réalisation de ce test, parlez-en à votre médecin traitant.";
             } else {
